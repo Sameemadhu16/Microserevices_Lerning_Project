892be3fde1c752883cd20521d42ecbb7656600be
P.D.S.MADHURANGA <2022cs113@stu.ucsc.cmb.ac.lk>
2025-11-28 19:09:07 +0530

    feat: Add Swagger UI and security configuration for Inventory service

--- inventory/src/main/java/com/example/inventory/InventoryApplication.java
+++ inventory/src/main/java/com/example/inventory/InventoryApplication.java
@@ -1,7 +1,12 @@
 package com.example.inventory;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @SpringBootApplication
 public class InventoryApplication {
@@ -9,5 +14,19 @@ public class InventoryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryApplication.class, args);
 	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+	@Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Microservices POS System - Inventory Service API")
+                        .version("v0.1")
+                        .description("This is the Inventory Service API documentation for the Microservices POS System."));
+    }
 
 }

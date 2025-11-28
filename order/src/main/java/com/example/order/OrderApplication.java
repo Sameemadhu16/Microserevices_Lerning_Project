72a9b9483c4255b0133f3b1ba3fe5a0cd37f45dc
P.D.S.MADHURANGA <2022cs113@stu.ucsc.cmb.ac.lk>
2025-11-28 19:43:38 +0530

    feat: Add Swagger UI integration and development security configuration for Order service

--- order/src/main/java/com/example/order/OrderApplication.java
+++ order/src/main/java/com/example/order/OrderApplication.java
@@ -1,7 +1,12 @@
 package com.example.order;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @SpringBootApplication
 public class OrderApplication {
@@ -9,5 +14,19 @@ public class OrderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
 	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+	
+	@Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Microservices POS System - Order Service API")
+                        .version("v0.1")
+                        .description("This is the Order Service API documentation for the Microservices POS System."));
+    }
 
 }
